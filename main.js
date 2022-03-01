682eb491f5ef541a82504d401b7cd7d9251318ce
Dani Ryuu Carmona <DaniiCarmona22@gmail.com>
2022-03-01 23:04:35 +0900

    First commit

---
+++ main.js
@@ -0,0 +1,44 @@
+const qrcode = require('qrcode-terminal');
+
+const { Client } = require('whatsapp-web.js');
+const client = new Client();
+
+// Get phone number from cli arguments
+const phonenumber = process.argv[2];
+
+// Get desired time from cli arguments
+const desiredTime = process.argv[3];
+
+// Get Message from CLI
+const message = process.argv[4];
+
+client.on('qr', qr => {
+    qrcode.generate(qr, {small: true});
+});
+
+client.on('ready', () => {
+    setInterval(attemptToSendMessage, 1000);
+});
+
+client.on('message', message => {
+	console.log(message.body);
+});
+
+client.initialize();
+
+function attemptToSendMessage() {
+   if(isItTime()) {
+        client.sendMessage(`34${phonenumber}@c.us`, message);
+        console.log('NIGHTMARE SLAIN');
+   }
+}
+
+function isItTime() {
+    var today = new Date();
+    var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+    if (time == desiredTime) {
+        return true;
+    }
+    console.log('Not time yet', time);
+    return false;
+}
